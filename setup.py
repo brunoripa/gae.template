f99302bba2b898a502f90d825f157c7d29b18c96
Bruno Ripa <bruno.ripa@gmail.com>
2015-02-19 21:47:43 +0000

    Fixed a typo.

--- setup.py
+++ setup.py
@@ -3,22 +3,22 @@ from setuptools import setup, find_packages
 version = "0.1.1"
 
 setup(name="gae.template",
-    version=version,
-    description="GAE Application template for PasteScript",
-    long_description="""Long description""",
-    classifiers=[],
-    keywords="",
-    author="Bruno Ripa",
-    author_email="bruno.ripa@gmail.com",
-    url="",
-    license="BSD",
-    packages=find_packages(),
-    py_modules=["template",],
-    include_package_data=True,
-    zip_safe=True,
-    install_requires=["PasteScript>=1.3"],
-    entry_points="""
-    [paste.paster_create_template]
-    gae_project=template:GaeProjectTemplate
-    """
+      version=version,
+      description="GAE Application template for PasteScript",
+      long_description="""Long description""",
+      classifiers=[],
+      keywords="",
+      author="Bruno Ripa",
+      author_email="bruno.ripa@gmail.com",
+      url="",
+      license="BSD",
+      packages=find_packages(),
+      py_modules=["template",],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=["PasteScript>=1.3"],
+      entry_points="""
+        [paste.paster_create_template]
+        gae_project=template:GaeProjectTemplate
+        """
 )
